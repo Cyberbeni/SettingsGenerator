32f6d272be535e0a097c02a02c43e3d49c77518b
Benedek Kozma <cyberbeni@gmail.com>
2018-05-22 23:05:28 +0200

    Initial Commit

---
+++ SettingsGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SettingsGenerator
+//
+//  Created by Benedek Kozma on 2018. 05. 22..
+//  Copyright © 2018. Benedek Kozma. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
